dca1066055c6e6aa909596935229465ce38b5f21
Sharlene Tio <sharlenetio.2021@scis.smu.edu.sg>
2024-03-15 20:56:43 +0800

    initialised project

---
+++ app/src/main/java/com/example/cs205proj/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.cs205proj;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
