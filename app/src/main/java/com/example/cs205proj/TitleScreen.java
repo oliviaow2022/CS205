e5e7333c87500af7cf6a99ebff359afec750726a
Ryan <ryanlimjk@gmail.com>
2024-03-18 22:07:39 +0800

    WE HAVE BACKGROUND IN THE TITLESCREEN

--- app/src/main/java/com/example/cs205proj/TitleScreen.java
+++ app/src/main/java/com/example/cs205proj/TitleScreen.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class TitleScreen extends AppCompatActivity {
 
@@ -14,6 +15,9 @@ public class TitleScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title_screen);
         Button buttonStart = findViewById(R.id.button_start);
+
+        // Background GIF
+
         buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
