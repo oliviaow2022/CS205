9b64ffe619e703a229c791b813c2c0a0b93b9737
olivia <111635889+oliviaow2022@users.noreply.github.com>
2024-03-22 16:59:42 +0800

    game over screen

---
+++ app/src/main/java/com/example/cs205proj/GameOver.java
@@ -0,0 +1,25 @@
+package com.example.cs205proj;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+
+public class GameOver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_over);
+        Button buttonStart = findViewById(R.id.button_start);
+        buttonStart.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GameOver.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
